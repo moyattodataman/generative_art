cbdf89ebed76c86fd8e2a4defb22a892537cac66
モヤっとデータサイエンティスト <107286388+moyattodataman@users.noreply.github.com>
2022-09-04 15:17:41 +0900

    Add files via upload

--- generate.py
+++ generate.py
@@ -2,21 +2,27 @@ import random
 import os
 from PIL import Image
 
+# Set components directory and output directory
 components_directory_path = 'components'
 output_directory_path = 'output'
 
-# How many files created? Check it is below max patterns
+# Get layers list in components directory
 layers = os.listdir(components_directory_path)
 layers.sort()
 
+# Calculate max patterns 
 max_patterns = 1
 for layer in layers:
     layer_path = os.path.join(components_directory_path, layer)
     max_patterns = max_patterns * len(os.listdir(layer_path))
 
-count = max_patterns + 1
-while not count < max_patterns:
-    count = input(f'How many files created? Max {max_patterns}\n')
+# Set how many files to create
+count = input(f'How many files created? Max {max_patterns}\n')
+count = int(count)
+
+# If larger than max_patterns, re-enter
+while count > max_patterns:
+    count = input(f'Set a value less than Max {max_patterns}\n')
     count = int(count)
 
 # Generate images
